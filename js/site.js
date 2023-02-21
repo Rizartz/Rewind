4a3e700b9962d09effc4bed7da3fa7fcf38ec74b
Ridwan Khondaker <34105086+Rizartz@users.noreply.github.com>
2023-02-21 14:28:48 -0500

    Javascript and html updated
    Created forloop in javascript to reverse a string

--- js/site.js
+++ js/site.js
@@ -1,5 +1,35 @@
+// Get the string from the page
+//controller function
+function getValue(){
+    
+    document.getElementById("alert").classList.add("invisible");
+    
+    let userString = document.getElementById("userString").value;
+    let revString = reverseString(userString);
+    displayString(revString);
+}
 
-//Call Hello World
-function HelloWorld(){
-    alert("Hello World");
+//Reverse the string
+//logic function
+function reverseString(userString){
+    
+    let revString = [];
+
+    //reverse a string using a for loop
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];
+        
+    }
+    return revString;
+}
+
+//Display the reversed string to the user
+//view function
+function displayString(revString){
+
+    //write to page 
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+
+    //turn on the alert
+    document.getElementById("alert").classList.remove("invisible");
 }
